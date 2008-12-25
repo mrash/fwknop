e113cb7e8ab3e6f06b014efcbfd39ca076dbece0
Damien Stuart <dstart@dstuart.org>
2008-12-25 01:26:23 +0000

    Updates to allow for building libfko as a shared lib. (make use of libtool).
    git-svn-id: file:///home/mbr/svn/fwknop/trunk@17 510a4753-2344-4c79-9c09-4d669213fbeb

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@
 # Run this script to generate all the initial makefiles, etc.
 #
 aclocal -I config
-libtoolize --automake
+libtoolize --automake --copy --force
 autoheader
 automake --add-missing --copy --foreign
 autoconf
