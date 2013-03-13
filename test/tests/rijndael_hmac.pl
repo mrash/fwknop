3ef3ab29c87f307d10dccf2d9857dd4aacc687de
Michael Rash <mbr@cipherdyne.org>
2013-03-12 23:20:12 -0400

    [test suite] 'key_file' hash key update for HMAC SHA384 test

--- test/tests/rijndael_hmac.pl
+++ test/tests/rijndael_hmac.pl
@@ -282,7 +282,7 @@
             "-d $default_digest_file -p $default_pid_file $intf_str",
         'fw_rule_created' => $NEW_RULE_REQUIRED,
         'fw_rule_removed' => $NEW_RULE_REMOVED,
-        'key_file' => $cf{'rc_hmac_b64_key'},
+        'key_file' => $cf{'rc_hmac_sha384_key'},
         'fatal'    => $NO
     },
 
