fc8a74131bbb804a73a9b6e49371e7393459d8c5
Michael Rash <mbr@cipherdyne.org>
2013-06-12 23:10:19 -0400

    [test suite] minor OS compatibility test re-order

--- test/tests/os_compatibility.pl
+++ test/tests/os_compatibility.pl
@@ -1,6 +1,5 @@
 @os_compatibility = (
 
-    ### Ubuntu 12.04
     {
         'category' => 'Rijndael',
         'subcategory' => 'client->server OS compatibility',
@@ -20,6 +19,61 @@
         'fatal'    => $NO
     },
     {
+        'category' => 'Rijndael',
+        'subcategory' => 'client->server OS compatibility',
+        'detail'   => 'v2.5 Ubuntu-13.04',
+        'function' => \&os_compatibility,
+        'no_ip_check' => 1,
+        'pkt' =>
+            '/gNh7EXXyg23dcrYz0g+KaWJWvTwuPuP349YzrwcLVvVSc5RVJdakdx9Qv0xAWe' .
+            'GsZJPmv2e1U31SMrdgF+o7/f2qRDH2hwPU8XLKS73rXpAhZKVAF/crt00HDmaH0' .
+            'p+hc3ngPtmE/j0PKeUD+GM81YQPO9NdZu4s',
+        'server_positive_output_matches' => [qr/with expire time/],
+        'fwknopd_cmdline'  => "LD_LIBRARY_PATH=$lib_dir $valgrind_str " .
+            "$fwknopdCmd -c $cf{'disable_aging'} -a $cf{'def_access'} " .
+            "-d $default_digest_file -p $default_pid_file $intf_str",
+        'fw_rule_created' => $NEW_RULE_REQUIRED,
+        'fw_rule_removed' => $NEW_RULE_REMOVED,
+        'fatal'    => $NO
+    },
+    {
+        'category' => 'Rijndael',
+        'subcategory' => 'client->server OS compatibility',
+        'detail'   => 'v2.5 FreeBSD-8.2',
+        'function' => \&os_compatibility,
+        'no_ip_check' => 1,
+        'pkt' =>
+            '9oprfzL5On8uyH5qJ0JQGLGddfGMI8j9YSafjFth2hiJrYG75FqBJNVvnsvetTA' .
+            '46kFmbdXHZCUx5iom9jOtpQnMvZJGex65vV4bSFdVwaoJ/ICkiRHbbzSTZo8qmp' .
+            'FTLSYWVhTWQddj4j80Ne6GH0h3zXomg9fJU',
+        'server_positive_output_matches' => [qr/with expire time/],
+        'fwknopd_cmdline'  => "LD_LIBRARY_PATH=$lib_dir $valgrind_str " .
+            "$fwknopdCmd -c $cf{'disable_aging'} -a $cf{'def_access'} " .
+            "-d $default_digest_file -p $default_pid_file $intf_str",
+        'fw_rule_created' => $NEW_RULE_REQUIRED,
+        'fw_rule_removed' => $NEW_RULE_REMOVED,
+        'fatal'    => $NO
+    },
+    {
+        'category' => 'Rijndael',
+        'subcategory' => 'client->server OS compatibility',
+        'detail'   => 'v2.5 OpenBSD-4.9',
+        'function' => \&os_compatibility,
+        'no_ip_check' => 1,
+        'pkt' =>
+            '+HNnKhqILxDz1IhEeJnWIQF2v/7H7yZ3TZegUeNltMdUP7SYAHxWg8WD2N4LF3g' .
+            'dejv3UxC/FRlHgJz4UeRkloFCQQ0tkQLx6MSoCQHKPlNxATKsfLL3UfHpKbhRG8' .
+            'a8S9q8lojKxiWuLZU64h5LXjhH7rR7riyds',
+        'server_positive_output_matches' => [qr/with expire time/],
+        'fwknopd_cmdline'  => "LD_LIBRARY_PATH=$lib_dir $valgrind_str " .
+            "$fwknopdCmd -c $cf{'disable_aging'} -a $cf{'def_access'} " .
+            "-d $default_digest_file -p $default_pid_file $intf_str",
+        'fw_rule_created' => $NEW_RULE_REQUIRED,
+        'fw_rule_removed' => $NEW_RULE_REMOVED,
+        'fatal'    => $NO
+    },
+
+    {
         'category' => 'Rijndael+HMAC',
         'subcategory' => 'client->server OS compatibility',
         'detail'   => 'v2.5 Ubuntu-12.04',
@@ -33,26 +87,6 @@
         'server_positive_output_matches' => [qr/with expire time/],
         'fwknopd_cmdline'  => "LD_LIBRARY_PATH=$lib_dir $valgrind_str " .
             "$fwknopdCmd -c $cf{'disable_aging'} -a $cf{'hmac_access'} " .
-            "-d $default_digest_file -p $default_pid_file $intf_str",
-        'fw_rule_created' => $NEW_RULE_REQUIRED,
-        'fw_rule_removed' => $NEW_RULE_REMOVED,
-        'fatal'    => $NO
-    },
-
-    ### Ubuntu 13.04
-    {
-        'category' => 'Rijndael',
-        'subcategory' => 'client->server OS compatibility',
-        'detail'   => 'v2.5 Ubuntu-13.04',
-        'function' => \&os_compatibility,
-        'no_ip_check' => 1,
-        'pkt' =>
-            '/gNh7EXXyg23dcrYz0g+KaWJWvTwuPuP349YzrwcLVvVSc5RVJdakdx9Qv0xAWe' .
-            'GsZJPmv2e1U31SMrdgF+o7/f2qRDH2hwPU8XLKS73rXpAhZKVAF/crt00HDmaH0' .
-            'p+hc3ngPtmE/j0PKeUD+GM81YQPO9NdZu4s',
-        'server_positive_output_matches' => [qr/with expire time/],
-        'fwknopd_cmdline'  => "LD_LIBRARY_PATH=$lib_dir $valgrind_str " .
-            "$fwknopdCmd -c $cf{'disable_aging'} -a $cf{'def_access'} " .
             "-d $default_digest_file -p $default_pid_file $intf_str",
         'fw_rule_created' => $NEW_RULE_REQUIRED,
         'fw_rule_removed' => $NEW_RULE_REMOVED,
@@ -77,21 +111,20 @@
         'fw_rule_removed' => $NEW_RULE_REMOVED,
         'fatal'    => $NO
     },
-
-    ### OpenBSD 4.9
     {
-        'category' => 'Rijndael',
+        'category' => 'Rijndael+HMAC',
         'subcategory' => 'client->server OS compatibility',
-        'detail'   => 'v2.5 OpenBSD-4.9',
+        'detail'   => 'v2.5 FreeBSD-8.2',
         'function' => \&os_compatibility,
         'no_ip_check' => 1,
         'pkt' =>
-            '+HNnKhqILxDz1IhEeJnWIQF2v/7H7yZ3TZegUeNltMdUP7SYAHxWg8WD2N4LF3g' .
-            'dejv3UxC/FRlHgJz4UeRkloFCQQ0tkQLx6MSoCQHKPlNxATKsfLL3UfHpKbhRG8' .
-            'a8S9q8lojKxiWuLZU64h5LXjhH7rR7riyds',
+            '8au7bGF/FL3EwjMVtTQpAtrzW4JvYAlAtbuEHGdxYS0E0vo18t9dpKoYA/hCLMu' .
+            'XqFYIH6zINNYUYjplvL9+zFeygIxiT9dm6PIjjossOIRRNOwdwbS8oeBzsckqMn' .
+            'BDo3d0K5I4mhwuWVHyTZUg441+kfm7O8TesDhUoy2ftqTGR3+GHi52/NIVctEAp' .
+            'WR6NZowCcHElB9E',
         'server_positive_output_matches' => [qr/with expire time/],
         'fwknopd_cmdline'  => "LD_LIBRARY_PATH=$lib_dir $valgrind_str " .
-            "$fwknopdCmd -c $cf{'disable_aging'} -a $cf{'def_access'} " .
+            "$fwknopdCmd -c $cf{'disable_aging'} -a $cf{'hmac_access'} " .
             "-d $default_digest_file -p $default_pid_file $intf_str",
         'fw_rule_created' => $NEW_RULE_REQUIRED,
         'fw_rule_removed' => $NEW_RULE_REMOVED,
@@ -116,44 +149,4 @@
         'fw_rule_removed' => $NEW_RULE_REMOVED,
         'fatal'    => $NO
     },
-
-    ### FreeBSD 8.2
-    {
-        'category' => 'Rijndael',
-        'subcategory' => 'client->server OS compatibility',
-        'detail'   => 'v2.5 FreeBSD-8.2',
-        'function' => \&os_compatibility,
-        'no_ip_check' => 1,
-        'pkt' =>
-            '9oprfzL5On8uyH5qJ0JQGLGddfGMI8j9YSafjFth2hiJrYG75FqBJNVvnsvetTA' .
-            '46kFmbdXHZCUx5iom9jOtpQnMvZJGex65vV4bSFdVwaoJ/ICkiRHbbzSTZo8qmp' .
-            'FTLSYWVhTWQddj4j80Ne6GH0h3zXomg9fJU',
-        'server_positive_output_matches' => [qr/with expire time/],
-        'fwknopd_cmdline'  => "LD_LIBRARY_PATH=$lib_dir $valgrind_str " .
-            "$fwknopdCmd -c $cf{'disable_aging'} -a $cf{'def_access'} " .
-            "-d $default_digest_file -p $default_pid_file $intf_str",
-        'fw_rule_created' => $NEW_RULE_REQUIRED,
-        'fw_rule_removed' => $NEW_RULE_REMOVED,
-        'fatal'    => $NO
-    },
-    {
-        'category' => 'Rijndael+HMAC',
-        'subcategory' => 'client->server OS compatibility',
-        'detail'   => 'v2.5 FreeBSD-8.2',
-        'function' => \&os_compatibility,
-        'no_ip_check' => 1,
-        'pkt' =>
-            '8au7bGF/FL3EwjMVtTQpAtrzW4JvYAlAtbuEHGdxYS0E0vo18t9dpKoYA/hCLMu' .
-            'XqFYIH6zINNYUYjplvL9+zFeygIxiT9dm6PIjjossOIRRNOwdwbS8oeBzsckqMn' .
-            'BDo3d0K5I4mhwuWVHyTZUg441+kfm7O8TesDhUoy2ftqTGR3+GHi52/NIVctEAp' .
-            'WR6NZowCcHElB9E',
-        'server_positive_output_matches' => [qr/with expire time/],
-        'fwknopd_cmdline'  => "LD_LIBRARY_PATH=$lib_dir $valgrind_str " .
-            "$fwknopdCmd -c $cf{'disable_aging'} -a $cf{'hmac_access'} " .
-            "-d $default_digest_file -p $default_pid_file $intf_str",
-        'fw_rule_created' => $NEW_RULE_REQUIRED,
-        'fw_rule_removed' => $NEW_RULE_REMOVED,
-        'fatal'    => $NO
-    },
-
 );
