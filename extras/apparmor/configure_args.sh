10ac35b344136a178511a2194926e683b382b5cd
Michael Rash <mbr@cipherdyne.org>
2013-11-12 23:26:54 -0500

    added extras/apparmor configure_args.sh helper script for building fwknop with args that AppArmor expects

---
+++ extras/apparmor/configure_args.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+#
+# This is a convenience script to run ./configure with the command line args
+# that the AppArmor policy expects (sets up binary locations, sysconfdir,
+# etc.).  Execute this script from the top level fwknop sources directory.
+#
+
+if [ -x ../configure ]; then
+    ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var
+else
+    echo "[*] Execute from the fwknop top level sources directory"
+fi
