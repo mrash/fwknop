cd8a2493a7d0679bc2c7e02d49ed46c3831972bf
Michael Rash <mbr@cipherdyne.org>
2014-04-22 23:20:06 -0400

    [test suite] python fuzzer additional tests

--- test/spa_fuzzing.py
+++ test/spa_fuzzing.py
@@ -21,6 +21,13 @@ import argparse
 
 def main():
 
+    ### a few constants
+    spa_success = 1
+    spa_failure = 0
+    spa_sha256  = 3
+    do_digest   = 1
+    no_digest   = 0
+
     args = parse_cmdline()
 
     print_hdr()
@@ -29,17 +36,65 @@ def main():
 
     pkt_id = 1
 
-    #### non-ascii char tests
+    ### valid payload tests - all digest types
+    print "# valid payloads..."
+    for digest_type in range(0, 6):
+        print str(pkt_id), str(spa_success), str(do_digest), \
+                str(digest_type), base64.b64encode(spa_payload)
+        pkt_id += 1
+
+    ### invalid digest types
+    print "# invalid digest types..."
+    for digest_type in [-1, 6, 7]:
+        print str(pkt_id), str(spa_success), str(do_digest), \
+                str(digest_type), base64.b64encode(spa_payload)
+        pkt_id += 1
+
+    ### truncated lengths
+    print "# truncated lengths..."
+    for l in range(1, len(spa_payload)):
+        print str(pkt_id), str(spa_failure), str(do_digest), \
+                str(spa_sha256), base64.b64encode(spa_payload[:l])
+        pkt_id += 1
+    for l in range(1, len(spa_payload)):
+        print str(pkt_id), str(spa_failure), str(do_digest), \
+                str(spa_sha256), base64.b64encode(spa_payload[l:])
+        pkt_id += 1
+
+    ### SPA payloads that are too long
+    print "# payloads too long..."
+    for l in [1, 10, 50, 127, 128, 129, 200, 399, \
+            400, 401, 500, 800, 1000, 1023, 1024, 1025, \
+            1200, 1499, 1500, 1501, 2000]:
+        for non_ascii in range(0, 5) + range(127, 130) + range(252, 255):
+            new_payload = spa_payload
+            for p in range(0, l):
+                new_payload += chr(non_ascii)
+            print str(pkt_id), str(spa_failure), str(do_digest), \
+                    str(spa_sha256), base64.b64encode(new_payload)
+            pkt_id += 1
+
+    ### additional embedded ':' chars
+    print "# additional embedded : chars..."
+    for pos in range(0, len(spa_payload)):
+        if spa_payload[pos] == ':':
+            continue
+        new_payload = list(spa_payload)
+        new_payload[pos] = ':'
+        print str(pkt_id), str(spa_failure), str(do_digest), \
+                str(spa_sha256), base64.b64encode(''.join(new_payload))
+        pkt_id += 1
+
+    ### non-ascii char tests
+    print "# non-ascii char tests..."
     for pos in range(0, len(spa_payload)):
         for non_ascii in range(0, 31) + range(127, 255):
             new_payload = list(spa_payload)
             new_payload[pos] = chr(non_ascii)
             ### write out the fuzzing line
-            print str(pkt_id) + " 0 1 3 " + base64.b64encode(''.join(new_payload))
+            print str(pkt_id), str(spa_failure), str(do_digest), \
+                    str(spa_sha256), base64.b64encode(''.join(new_payload))
             pkt_id += 1
-
-    # for c in list(spa_payload):
-        # print c
 
     return
 
