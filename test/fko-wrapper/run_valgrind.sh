55a03f33927dd95719dbe0683a3b29b6d3501344
Michael Rash <mbr@cipherdyne.org>
2014-06-15 10:34:52 -0400

    [test suite] added suppressions to fko-wrapper/run_valgrind.sh

--- test/fko-wrapper/run_valgrind.sh
+++ test/fko-wrapper/run_valgrind.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -x
 
 if [ $@ ]; then
-    LD_LIBRARY_PATH=../../lib/.libs valgrind --leak-check=full --show-reachable=yes --track-origins=yes $@
+    LD_LIBRARY_PATH=../../lib/.libs valgrind --leak-check=full \
+        --show-reachable=yes --track-origins=yes --suppressions=../valgrind_suppressions $@
 else
     echo "[*] Usage: ./run_valgrind.sh ./<binary>"
 fi
