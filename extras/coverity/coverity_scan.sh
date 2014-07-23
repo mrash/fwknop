5fadf56af42a6b320a5752cfb048df4697fb190e
Michael Rash <mbr@cipherdyne.org>
2014-07-22 22:05:29 -0400

    added extras/coverity/ directory for Coverity script

---
+++ extras/coverity/coverity_scan.sh
@@ -0,0 +1,10 @@
+#!/bin/sh +x
+
+make clean
+export PATH=$PATH:/home/mbr/coverity/cov-analysis-linux64-7.5.0/bin
+cov-build --dir cov-int make
+tar cvfz fwknop.tgz cov-int
+
+echo "[+] Submit build tarball (./fwknop.tgz) here: http://scan.coverity.com/upload.html"
+
+exit
