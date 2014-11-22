c1a1a4d3aaf563917c7ee3dafaac1f1b124a0b8b
Michael Rash <mbr@cipherdyne.org>
2014-11-21 21:22:20 -0500

    [test suite] minor comment removal

--- test/afl/fuzzing-wrappers/fwknopd-stdin-test.sh
+++ test/afl/fuzzing-wrappers/fwknopd-stdin-test.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -x
 
 SPA_PKT="1716411011200157:root:1397329899:2.0.1:1:127.0.0.2,tcp/22:AAAAA"
-#SPA_PKT="1716411011200157:root:1397329899:2.0.1:1:127.0.0.2,tcp/22:A"
 
 echo -n $SPA_PKT | LD_LIBRARY_PATH=../../lib/.libs ../../server/.libs/fwknopd -c ../conf/default_fwknopd.conf -a ../conf/default_access.conf -A -f -t
