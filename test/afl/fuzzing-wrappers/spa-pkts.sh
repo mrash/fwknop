28abc276fe0daae16d3e3fe642ab9946fc858108
Michael Rash <mbr@cipherdyne.org>
2014-11-23 21:01:44 -0500

    [test suite] AFL update spa-pkts.sh to use new shared bash functions

--- test/afl/fuzzing-wrappers/spa-pkts.sh
+++ test/afl/fuzzing-wrappers/spa-pkts.sh
@@ -1,22 +1,28 @@
 #!/bin/sh -x
 
-TOP_DIR="fuzzing-output"
+#
+# Fuzz SPA packet encoding/decoding
+#
+
+. ./fuzzing-wrappers/fcns
+
 FDIR="spa-pkts.out"
 ARCHIVE_DIR="$TOP_DIR/archive"
 OUT_DIR="$TOP_DIR/$FDIR"
 IN_DIR="test-cases/spa-pkts"
 
-SERVER="../../server/.libs/fwknopd"
-LIB_DIR="../../lib/.libs"
+### build up our afl-fuzz text banner
+TSTR="fwknopd,SPA,encode/decode"
+GIT_STR=''
+git_banner GIT_STR
+BANNER="$TSTR$GIT_STR"
 
-[ ! -d $ARCHIVE_DIR ] && mkdir -p $ARCHIVE_DIR
-TS=`date +"%m%d%y%H%M%S"`
-[ -d $OUT_DIR ] && mv $OUT_DIR "$ARCHIVE_DIR/$FDIR-$TS"
-mkdir $OUT_DIR
+### set up directories
+dir_init $ARCHIVE_DIR $FDIR $OUT_DIR
 
 ### make sure that a basic SPA packet to stdin in fwknopd -A mode works
 ./fuzzing-wrappers/helpers/fwknopd-stdin-test.sh || exit $?
 
-LD_LIBRARY_PATH=$LIB_DIR afl-fuzz -T "fwknopd SPA packets" -t 1000 -i $IN_DIR -o $OUT_DIR $SERVER -c ../conf/default_fwknopd.conf -a ../conf/default_access.conf -A -f -t
+LD_LIBRARY_PATH=$LIB_DIR afl-fuzz -T $BANNER -t 1000 -i $IN_DIR -o $OUT_DIR $SERVER -c ../conf/default_fwknopd.conf -a ../conf/default_access.conf -A -f -t
 
 exit $?
