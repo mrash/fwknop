1f454622064da8db625f4e1833c83a63c403a80f
Michael Rash <mbr@cipherdyne.org>
2015-05-21 22:18:14 -0400

    [extras] run ramdisk creation script from top level sources directory

--- extras/ramdisk/ramdisk-create.sh
+++ extras/ramdisk/ramdisk-create.sh
@@ -1,8 +1,26 @@
 #!/bin/sh -x
+#
+# This script creates a ramdisk and clones the fwknop git repository
+# into it. This is meant for AFL fuzzing cycles.
+#
 
+RSIZE=768M
 RAMDISK=/tmp/afl-ramdisk
-mkdir $RAMDISK && chmod 777 $RAMDISK
-mount -t tmpfs -o size=768M tmpfs $RAMDISK
 
-cd ../.. && git clone . $RAMDISK/fwknop.git
+if [ -d $RAMDISK ]
+then
+    echo "[*] $RAMDISK directory already exists"
+    exit 1
+fi
+
+if [ -f extras/ramdisk/ramdisk-create.sh ]
+then
+    mkdir $RAMDISK && chmod 777 $RAMDISK
+    mount -t tmpfs -o size=$RSIZE tmpfs $RAMDISK
+    git clone . $RAMDISK/fwknop.git
+else
+    echo "[*] Run this script from the top level fwknop sources directory"
+    exit 1
+fi
+
 exit $?
