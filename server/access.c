3388d85073f268134f86d318b48fc56224f0c769
Michael Rash <mbr@cipherdyne.org>
2015-12-17 21:27:13 -0500

    Revert "[server] access_folder is guaranteed to be NULL terminated"
    This reverts commit 24f57f36a742458b204635cd3dbf8003fa3a3eb8.

--- server/access.c
+++ server/access.c
@@ -1349,6 +1349,7 @@ acc_data_is_valid(fko_srv_options_t *opts,
 int
 parse_access_folder(fko_srv_options_t *opts, char *access_folder, int *depth)
 {
+    char            *ndx;
     char            *extension;
     DIR             *dir_ptr;
 
@@ -1361,8 +1362,11 @@ parse_access_folder(fko_srv_options_t *opts, char *access_folder, int *depth)
         acc_stanza_init(opts);
     }
 
-    /* access_folder is guaranteed to be NULL terminated
-    */
+    if((ndx = strrchr(access_folder, '/')) != NULL)
+    {
+        if (strlen(ndx) == 1)
+            *ndx = '\0';
+    }
     dir_ptr = opendir(access_folder);
 
     //grab the file names in the directory and loop through them
