5e6530cc237e03ee18e4c0950afed7c01f1696af
Michael Rash <mbr@cipherdyne.org>
2016-01-11 22:22:02 -0500

    [test suite] bug fix in lcov coverage generation to pull in _base and _info into the final result

--- test/gen-coverage-report.sh
+++ test/gen-coverage-report.sh
@@ -5,8 +5,8 @@
 cd ..
 
 lcov $LCOV_RC_BC --no-checksum --capture --directory . --output-file $LCOV_INFO
-lcov $LCOV_RC_BC --no-checksum -a $LCOV_BASE -a $LCOV_INFO --output-file $LCOV_INFO_FINAL
-lcov $LCOV_RC_BC --no-checksum -r $LCOV_INFO /usr/include/\* --output-file $LCOV_INFO_FINAL
+lcov $LCOV_RC_BC --no-checksum -a $LCOV_BASE -a $LCOV_INFO --output-file $LCOV_INFO_TMP
+lcov $LCOV_RC_BC --no-checksum -r $LCOV_INFO_TMP /usr/include/\* --output-file $LCOV_INFO_FINAL
 genhtml $GENHTML_USE_BC --output-directory $LCOV_RESULTS_DIR --branch-coverage $LCOV_INFO_FINAL
 
 cd $TOP_DIR
