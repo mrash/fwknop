84d4128812d147ab0667834d1c72ec4117fb438c
Michael Rash <mbr@cipherdyne.org>
2016-04-15 17:37:16 -0700

    [test suite] exclude test/fko-wrapper/* from code coverage stats (we want coverage for the client/server/lib code)

--- test/gen-coverage-report.sh
+++ test/gen-coverage-report.sh
@@ -6,7 +6,8 @@ cd ..
 
 lcov $LCOV_RC_BC --no-checksum --capture --directory . --output-file $LCOV_INFO
 lcov $LCOV_RC_BC --no-checksum -a $LCOV_BASE -a $LCOV_INFO --output-file $LCOV_INFO_TMP
-lcov $LCOV_RC_BC --no-checksum -r $LCOV_INFO_TMP /usr/include/\* --output-file $LCOV_INFO_FINAL
+lcov $LCOV_RC_BC --no-checksum -r $LCOV_INFO_TMP /usr/include/\* --output-file $LCOV_INFO_TMP2
+lcov $LCOV_RC_BC --no-checksum -r $LCOV_INFO_TMP2 test/fko-wrapper/\* --output-file $LCOV_INFO_FINAL
 genhtml $GENHTML_USE_BC --output-directory $LCOV_RESULTS_DIR --branch-coverage $LCOV_INFO_FINAL
 
 cd $TOP_DIR
