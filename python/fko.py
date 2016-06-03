a4879fdfc4822528b6ee79e7b773adfba08689db
Damien Stuart <dstuart@dstuart.org>
2016-06-02 22:36:59 -0400

    Added explicit GPL2 license text to the python module code files.

--- python/fko.py
+++ python/fko.py
@@ -1,3 +1,25 @@
+##############################################################################
+#
+# fko.py
+#
+#  License (GNU General Public License):
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+#  USA
+#
+##############################################################################
 """Wrapper functions for libfko.
 
 The fko module provides a class that implements the functions for
--- python/setup.py
+++ python/setup.py
@@ -9,6 +9,23 @@
 # Copyright (C) 2009-2014 fwknop developers and contributors. For a full
 # list of contributors, see the file 'CREDITS'.
 #
+# License (GNU General Public License):
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+# USA
+#
 ##############################################################################
 #
 from distutils.core import setup, Extension
@@ -30,7 +47,7 @@ setup (
     description = 'Wrapper for Fwknop library (libfko)',
     author = 'Damien S. Stuart',
     author_email = 'dstuart@dstuart.org',
-    license = 'GPL',
+    license = 'GPL2',
     long_description = '''
 Python module that wraps the fwknop library to provide the ability to
 generate, decode, parse, and process SPA formatted messages.
