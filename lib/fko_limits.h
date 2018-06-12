4c92ab116fc0d7b413756968e62f44a2a212a0ba
Pierre Pronchery <khorben@defora.org>
2018-06-12 16:22:25 -0400

    Let IPs resolve to IPv6 addresses
    The remote code seems to be independent from the fwknop project though.
    Until it will be capable to return IPv6 addresses, in itself this will
    remain irrelevant for the purpose of adding IPv6 support to fwknop.
    
    On another hand, it does help us introduce definitions and update
    headers to actually support IPv6.

--- lib/fko_limits.h
+++ lib/fko_limits.h
@@ -59,6 +59,12 @@
 #define MAX_IPV4_STR_LEN             16
 #define MIN_IPV4_STR_LEN              7
 
+#define MAX_IPV46_STR_LEN            40
+#define MIN_IPV46_STR_LEN             3
+
+#define MAX_IPV6_STR_LEN             40
+#define MIN_IPV6_STR_LEN              3
+
 #define MAX_PROTO_STR_LEN             4  /* tcp, udp, icmp for now */
 #define MAX_PORT_STR_LEN              5
 #define MAX_PORT                  65535
