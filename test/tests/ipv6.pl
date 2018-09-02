9c5069e03639416a667e649960c680da71fab128
Michael Rash <mbr@cipherdyne.org>
2018-09-02 16:02:28 -0700

    [test suite] first working IPv6 complete cycle test

--- test/tests/ipv6.pl
+++ test/tests/ipv6.pl
@@ -7,7 +7,7 @@
         'function' => \&spa_cycle,
         'cmdline'  => $default_client_hmac_args_ipv6,
         'fwknopd_cmdline' => "$fwknopdCmd -c $cf{'def'} -a $cf{'hmac_access'} " .
-            "-d $default_digest_file -p $default_pid_file $intf_str",
+            "--ipv6 -d $default_digest_file -p $default_pid_file $intf_str",
         'fw_rule_created' => $NEW_RULE_REQUIRED,
         'fw_rule_removed' => $NEW_RULE_REMOVED,
         'key_file' => $cf{'rc_hmac_b64_key'},
